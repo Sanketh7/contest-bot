d28ac99db953450471ba596d6f99cdeff9fda13e
Sanketh Varamballi <sankethv03@gmail.com>
2020-05-13 15:10:56 -0700

    Update main.py

--- source/main.py
+++ source/main.py
@@ -298,10 +298,11 @@ async def contest_schedule_loop():
         elif not states["is_contest_active"]:
             schedule = await db.get_scheduled_contest_list()
             curr_time = datetime.datetime.utcnow().timestamp()
-            for _, contest_data in schedule.items():
-                if float(contest_data["start_time"]) <= curr_time <= float(contest_data["end_time"]):
-                    await start_contest(contest_data["contest_type"], float(contest_data["end_time"]))
-                    break
+            if schedule is not None:
+                for _, contest_data in schedule.items():
+                    if float(contest_data["start_time"]) <= curr_time <= float(contest_data["end_time"]):
+                        await start_contest(contest_data["contest_type"], float(contest_data["end_time"]))
+                        break
 
         await asyncio.sleep(60)
 
