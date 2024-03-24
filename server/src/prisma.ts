01fc462a560c4eb527b0dc4ad8ebc145e70a409d
Sanketh Varamballi <22879134+Sanketh7@users.noreply.github.com>
2024-03-23 18:30:41 -0700

    added soft delete

--- server/src/prisma.ts
+++ server/src/prisma.ts
@@ -1,9 +1,26 @@
 import { PrismaClient } from "@prisma/client";
+import { createSoftDeleteExtension } from "prisma-extension-soft-delete";
 import { PointsManager } from "./pointsManager";
 
-export const prisma = new PrismaClient();
+const softDeleteExtension = createSoftDeleteExtension({
+  models: {
+    Contest: true,
+    Character: true,
+    Submission: true,
+    Guild: true,
+  },
+  defaultConfig: {
+    field: "deletedAt",
+    createValue: (deleted) => {
+      if (deleted) return new Date();
+      return null;
+    },
+  },
+});
 
-export const prismaWithPoints = new PrismaClient().$extends({
+export const prisma = new PrismaClient().$extends(softDeleteExtension);
+
+export const prismaWithPoints = new PrismaClient().$extends(softDeleteExtension).$extends({
   result: {
     character: {
       points: {
